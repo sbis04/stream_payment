361828ada744cf992ac1608a4c4d93e4208969e6
Souvik Biswas <sbis1999@gmail.com>
2021-07-06 01:39:41 +0530

    Add tx attachment

---
+++ lib/widgets/transaction_attachment.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TransactionAttachment extends StatelessWidget {
+  final int amount;
+
+  const TransactionAttachment({
+    Key? key,
+    required this.amount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFF4161ff),
+      width: double.maxFinite,
+      height: 300,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.payment,
+            color: Colors.white,
+            size: 40,
+          ),
+          SizedBox(width: 8),
+          Column(
+            children: [
+              Text(
+                '$amount USD',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}

b5658e5ed3d86fcd0e3622faa54201d0fa0d63d2
Souvik Biswas <sbis1999@gmail.com>
2021-07-06 01:38:54 +0530

    Add channel list page

---
+++ lib/screens/channel_list_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+
+import 'channel_page.dart';
+
+class ChannelListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stream Chat'),
+      ),
+      body: ChannelsBloc(
+        child: ChannelListView(
+          filter: Filter.in_('members', [StreamChat.of(context).user!.id]),
+          sort: [SortOption('last_message_at')],
+          pagination: PaginationParams(limit: 30),
+          channelWidget: Builder(
+            builder: (context) => ChannelPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}

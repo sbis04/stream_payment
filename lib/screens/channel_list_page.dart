f92394b1880fac69ec9dd08f630b6e2a465c64b8
Souvik Biswas <sbis1999@gmail.com>
2021-07-06 14:50:33 +0530

    Add constructor to take channel :smile:

--- lib/screens/channel_list_page.dart
+++ lib/screens/channel_list_page.dart
@@ -4,6 +4,10 @@ import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import 'channel_page.dart';
 
 class ChannelListPage extends StatelessWidget {
+  final Channel channel;
+
+  const ChannelListPage(this.channel);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +20,7 @@ class ChannelListPage extends StatelessWidget {
           sort: [SortOption('last_message_at')],
           pagination: PaginationParams(limit: 30),
           channelWidget: Builder(
-            builder: (context) => ChannelPage(),
+            builder: (context) => ChannelPage(channel),
           ),
         ),
       ),

a9950594f90a208e8402170f5ab8942383a247a0
Souvik Biswas <sbis1999@gmail.com>
2021-07-06 21:51:14 +0530

    Separate the transaction widget

--- lib/screens/channel_page.dart
+++ lib/screens/channel_page.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:loading_overlay/loading_overlay.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 import 'package:stream_payment/models/transfer.dart';
-import 'package:stream_payment/res/pallet.dart';
-import 'package:stream_payment/screens/detail_page.dart';
 import 'package:stream_payment/screens/transaction_page.dart';
 import 'package:stream_payment/utils/rapyd_client.dart';
 import 'package:stream_payment/widgets/transaction_attachment.dart';
+import 'package:stream_payment/widgets/transaction_widget.dart';
 
 class ChannelPage extends StatefulWidget {
   final Channel channel;
@@ -75,101 +74,11 @@ class _ChannelPageState extends State<ChannelPage> {
 
     return wrapAttachmentWidget(
       context,
-      InkWell(
-        onTap: () {
-          Navigator.of(context).push(
-            PageRouteBuilder(
-              opaque: false,
-              pageBuilder: (_, __, ___) => DetailPage(
-                transaction: transaction,
-              ),
-            ),
-          );
-        },
-        child: Container(
-          width: double.maxFinite,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(16.0),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  color: Pallet.streamBlue,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(16.0),
-                    topRight: Radius.circular(16.0),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Sending To',
-                        style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 16.0,
-                        ),
-                      ),
-                      SizedBox(height: 4),
-                      Text(
-                        destWalletAddress,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 12.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                color: Pallet.streamBlue.withOpacity(0.1),
-                width: double.maxFinite,
-                child: Center(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        '\$',
-                        style: TextStyle(fontSize: 50.0),
-                      ),
-                      Text(
-                        '$amount',
-                        style: TextStyle(fontSize: 80.0),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                width: double.maxFinite,
-                decoration: BoxDecoration(
-                  color: Pallet.streamBlue,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(16.0),
-                    bottomRight: Radius.circular(16.0),
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'STATUS: ${status == 'PEN' ? 'Pending' : status == 'DEC' ? 'Declined' : 'Completed'}',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
+      TransactionWidget(
+        transaction: transaction,
+        destWalletAddress: destWalletAddress,
+        amount: amount,
+        status: status,
       ),
       RoundedRectangleBorder(),
       true,
---
+++ lib/widgets/transaction_widget.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:stream_payment/models/transfer.dart';
+import 'package:stream_payment/res/pallet.dart';
+import 'package:stream_payment/screens/detail_page.dart';
+
+class TransactionWidget extends StatelessWidget {
+  const TransactionWidget({
+    Key? key,
+    required this.transaction,
+    required this.destWalletAddress,
+    required this.amount,
+    required this.status,
+  }) : super(key: key);
+
+  final Transfer transaction;
+  final String destWalletAddress;
+  final int amount;
+  final String status;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          PageRouteBuilder(
+            opaque: false,
+            pageBuilder: (_, __, ___) => DetailPage(
+              transaction: transaction,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        width: double.maxFinite,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Pallet.streamBlue,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sending To',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      destWalletAddress,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              color: Pallet.streamBlue.withOpacity(0.1),
+              width: double.maxFinite,
+              child: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      '\$',
+                      style: TextStyle(fontSize: 50.0),
+                    ),
+                    Text(
+                      '$amount',
+                      style: TextStyle(fontSize: 80.0),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: Pallet.streamBlue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(16.0),
+                  bottomRight: Radius.circular(16.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'STATUS: ${status == 'PEN' ? 'Pending' : status == 'DEC' ? 'Declined' : 'Completed'}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
